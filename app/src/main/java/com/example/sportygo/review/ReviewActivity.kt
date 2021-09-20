597cbbf244c3ccfb95c6b100931b965ce242f27c
UdithPremarathne <IT18137224@my.sliit.lk>
2021-09-20 20:31:25 +0530

    review ui done

--- app/src/main/java/com/example/sportygo/review/ReviewActivity.kt
+++ app/src/main/java/com/example/sportygo/review/ReviewActivity.kt
@@ -1,12 +1,22 @@
 package com.example.sportygo.review
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.sportygo.R
 
 class ReviewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_review)
+
+        val btnWriteReview = findViewById<Button>(R.id.btn_write_a_review)
+
+        btnWriteReview.setOnClickListener {
+            val intent = Intent(this, ReviewFormActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 }
---
+++ app/src/main/java/com/example/sportygo/review/ReviewFormActivity.kt
@@ -0,0 +1,13 @@
+
+package com.example.sportygo.review
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sportygo.R
+
+class ReviewFormActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_review_form)
+    }
+}
