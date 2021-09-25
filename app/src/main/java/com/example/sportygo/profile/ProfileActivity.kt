c3a319ba404a5f65618243f8ed3c4a42a6439a43
UdithPremarathne <IT18137224@my.sliit.lk>
2021-09-25 22:05:38 +0530

    All rewarding UI screens done

--- app/src/main/java/com/example/sportygo/profile/ProfileActivity.kt
+++ app/src/main/java/com/example/sportygo/profile/ProfileActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 import android.widget.LinearLayout
 import com.example.sportygo.R
 import com.example.sportygo.review.ReviewFormActivity
+import com.example.sportygo.rewards.RewardsActivity
 
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,9 +15,15 @@ class ProfileActivity : AppCompatActivity() {
         setContentView(R.layout.activity_profile)
 
         val linear_editProfile = findViewById<LinearLayout>(R.id.linear_editProfile)
+        val linear_rewards = findViewById<LinearLayout>(R.id.linear_rewards)
 
         linear_editProfile.setOnClickListener {
             val intent = Intent(this, EditProfileActivity::class.java)
+            startActivity(intent)
+        }
+
+        linear_rewards.setOnClickListener {
+            val intent = Intent(this, RewardsActivity::class.java)
             startActivity(intent)
         }
     }
---
+++ app/src/main/java/com/example/sportygo/rewards/PointActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sportygo.rewards
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sportygo.R
+
+class PointActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_point)
+    }
+}
