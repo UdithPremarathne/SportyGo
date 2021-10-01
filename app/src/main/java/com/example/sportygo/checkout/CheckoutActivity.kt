02d4194b0cd4a396ed6418e306d1e01fa0b4312e
thinthithili <43705673+thinthithili@users.noreply.github.com>
2021-10-01 20:50:47 +0530

    Checkout UI

--- app/src/main/java/com/example/sportygo/checkout/CheckoutActivity.kt
+++ app/src/main/java/com/example/sportygo/checkout/CheckoutActivity.kt
@@ -1,12 +1,37 @@
 package com.example.sportygo.checkout
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.example.sportygo.R
 
 class CheckoutActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_checkout)
+
+        //get the spinner from the xml.
+        //get the spinner from the xml.
+        val dropdown = findViewById<Spinner>(R.id.spinner2)
+//create a list of items for the spinner.
+//create a list of items for the spinner.
+        val items = arrayOf("42", "45", "47")
+//create an adapter to describe how the items are displayed, adapters are used in several places in android.
+//There are multiple variations of this, but this is the basic variant.
+//create an adapter to describe how the items are displayed, adapters are used in several places in android.
+//There are multiple variations of this, but this is the basic variant.
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, items)
+//set the spinners adapter to the previously created one.
+//set the spinners adapter to the previously created one.
+        dropdown.adapter = adapter
+
+
     }
+
+
+
+
 }
