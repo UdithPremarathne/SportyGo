7a519ba8c6e47979b3c21e59e37a9faee90393b4
Naweed <it18137774@my.sliit.lk>
2021-10-03 02:10:47 +0530

    home and category class

--- app/src/main/java/com/example/sportygo/home/HomeActivity.kt
+++ app/src/main/java/com/example/sportygo/home/HomeActivity.kt
@@ -2,11 +2,20 @@ package com.example.sportygo.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
 import com.example.sportygo.R
+import android.content.Intent
+import android.view.View
+
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(R.layout.activity_home2)
+    }
+
+    fun hotDlzClick(view: View?) {
+        val intent = Intent(this@HomeActivity, CategoryActivity::class.java)
+        startActivity(intent)
     }
 }
