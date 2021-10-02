9712654078273e8f5c9eb07f2e7d277842fa1c01
thinthithili <43705673+thinthithili@users.noreply.github.com>
2021-10-03 00:12:55 +0530

    Payment UI Corrected

--- app/src/main/java/com/example/sportygo/checkout/CheckoutActivity.kt
+++ app/src/main/java/com/example/sportygo/checkout/CheckoutActivity.kt
@@ -14,21 +14,29 @@ class CheckoutActivity : AppCompatActivity() {
         setContentView(R.layout.activity_checkout)
 
         //get the spinner from the xml.
-        //first dropdown
+        //ALL dropdowns
         val dropdown = findViewById<Spinner>(R.id.spinner2)
+        val dropdown1 = findViewById<Spinner>(R.id.spinner3)
+        val dropdown2 = findViewById<Spinner>(R.id.spinner)
+
 //create a list of items for the spinner.
-        val items = arrayOf("42", "45", "47")
+        val items = arrayOf("36", "38", "40", "42", "44", "45")
+        val items1 = arrayOf("Red", "Blue", "Green", "Purple")
+        val items2 = arrayOf("1", "2", "3", "4", "5")
 
 //create an adapter to describe how the items are displayed, adapters are used in several places in android.
 //There are multiple variations of this, but this is the basic variant.
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, items)
+        val adapter1 = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, items1)
+        val adapter2 = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, items2)
+
 //set the spinners adapter to the previously created one.
 
         dropdown.adapter = adapter
+        dropdown1.adapter = adapter1
+        dropdown2.adapter = adapter2
 
     }
 
-
-    //spinner3
 
 }
