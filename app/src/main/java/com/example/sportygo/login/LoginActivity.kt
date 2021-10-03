d8f901cad70669974b7676834f093b90f5b00b09
Vikum Chandrasekara <chandrasekara.vikum@gmail.com>
2021-10-03 09:48:17 +0530

    validations login and sign up

--- app/src/main/java/com/example/sportygo/login/LoginActivity.kt
+++ app/src/main/java/com/example/sportygo/login/LoginActivity.kt
@@ -4,11 +4,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.method.LinkMovementMethod
+import android.util.Patterns
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.sportygo.R
 import com.example.sportygo.databinding.ActivityLoginBinding
 import com.example.sportygo.databinding.ActivityMainBinding
+import com.example.sportygo.profile.EditProfileActivity
+import com.example.sportygo.profile.ProfileActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 
@@ -21,6 +26,8 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
         auth = FirebaseAuth.getInstance()
         val create_account = findViewById<TextView>(R.id.create_account)
+        val login = findViewById<TextView>(R.id.button)
+
 
         create_account.setOnClickListener {
 
@@ -30,7 +37,56 @@ class LoginActivity : AppCompatActivity() {
             create_account.movementMethod = LinkMovementMethod.getInstance();
 
         }
+
+        login.setOnClickListener {
+
+             doLogin()
+
         }
+    }
+
+    private fun doLogin() {
+
+            val name = findViewById<EditText>(R.id.name)
+            val email2 = findViewById<EditText>(R.id.email2)
+            val phoneNumber = findViewById<EditText>(R.id.phoneNumber)
+            val password = findViewById<EditText>(R.id.password)
+
+            if (email2.text.toString().isEmpty()) {
+                email2.error = "Please enter email"
+                email2.requestFocus()
+                return
+            }
+
+            if (!Patterns.EMAIL_ADDRESS.matcher(email2.text.toString()).matches()) {
+                email2.error = "Please enter valid email"
+                email2.requestFocus()
+                return
+            }
+
+            if (password.text.toString().isEmpty()) {
+                password.error = "Please enter password"
+                password.requestFocus()
+                return
+            }
+
+        auth.signInWithEmailAndPassword(email2.text.toString(),
+            password.text.toString()
+        )
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+
+
+                    updateUI(null)
+                }
+            }
+        }
+
+
+
     public override fun onStart() {
         super.onStart()
         // Check if user is signed in (non-null) and update UI accordingly.
@@ -39,7 +95,28 @@ class LoginActivity : AppCompatActivity() {
         updateUI(currentUser)
 
     }
-    fun updateUI(currentUser: FirebaseUser?) {}
+
+    private fun updateUI(currentUser: FirebaseUser?) {
+
+        if(currentUser != null){
+            if(currentUser.isEmailVerified){
+                startActivity(Intent(this,EditProfileActivity::class.java))
+                finish()
+            }else{
+                Toast.makeText(baseContext, "Please verify your email address",
+                    Toast.LENGTH_SHORT).show()
+
+            }
+
+
+        }else{
+
+            Toast.makeText(baseContext, "Login failed.",
+                Toast.LENGTH_SHORT).show()
+
+        }
+
+    }
 
     }
 
--- app/src/main/java/com/example/sportygo/login/SignupActivity.kt
+++ app/src/main/java/com/example/sportygo/login/SignupActivity.kt
@@ -1,16 +1,22 @@
 package com.example.sportygo.login
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PatternMatcher
 import android.text.TextUtils
 import android.text.method.LinkMovementMethod
+import android.util.Log
 import android.util.Patterns
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import com.example.sportygo.R
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+
 
 class SignupActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
@@ -19,9 +25,13 @@ class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
-        val goLogin = findViewById<TextView>(R.id.goLogin)
-        
 
+        val signupNow = findViewById<Button>(R.id.signupNow)
+
+        signupNow.setOnClickListener {
+            signUpUser()
+        }
+        val goLogin = findViewById<TextView>(R.id.goLogin)
         goLogin.setOnClickListener {
 
             val myIntent = Intent(this, LoginActivity::class.java)
@@ -31,6 +41,53 @@ class SignupActivity : AppCompatActivity() {
         }
     }
 
+    private fun signUpUser(){
+        val name = findViewById<EditText>(R.id.name)
+        val email2 = findViewById<EditText>(R.id.email2)
+        val phoneNumber = findViewById<EditText>(R.id.phoneNumber)
+        val password = findViewById<EditText>(R.id.password)
 
+        if (email2.text.toString().isEmpty()) {
+            email2.error = "Please enter email"
+            email2.requestFocus()
+            return
+        }
+
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email2.text.toString()).matches()) {
+            email2.error = "Please enter valid email"
+            email2.requestFocus()
+            return
+        }
+
+        if (password.text.toString().isEmpty()) {
+            password.error = "Please enter password"
+            password.requestFocus()
+            return
+        }
+
+        auth.createUserWithEmailAndPassword(email2.text.toString().isEmpty().toString(),
+            password.text.toString().isEmpty().toString()
+        )
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    user!!.sendEmailVerification()
+                        .addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                startActivity(Intent(this,LoginActivity::class.java))
+                                finish()
+                            }
+                        }
+
+                } else {
+
+                    Toast.makeText(baseContext, "SIgnUp failed.",
+                        Toast.LENGTH_SHORT).show()
+
+                }
+            }
+
+    }
 
 }
